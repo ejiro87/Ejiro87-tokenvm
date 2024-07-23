014e35698b4fc646709267e20063ceb9617af0b7
ejiro87 <louisejiro87@gmail.com>
2024-07-23 16:46:46 +0000

    louis

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "louis"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "lou"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "luis"
 )
 
 var ID ids.ID
